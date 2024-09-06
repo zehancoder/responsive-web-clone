e22be98451845defb64318e65416d2b18cc4d1bb
JH.ZEHAN CODER <zehanjh93@gmail.com>
2024-09-07 00:45:45 +0600

    Add files via upload

---
+++ web.js
@@ -0,0 +1,66 @@
+
+
+
+let main = document.querySelector(".main")
+
+let menuIcon = document.querySelector(".icon");
+
+let navToggle = document.querySelector(".responsive");
+
+let body = document.querySelector('body');
+
+let main2 = document.querySelector('.main2');
+
+let about = document.querySelector('.abouts');
+
+
+menuIcon.addEventListener("click", () => {
+    console.log("hi js");
+
+    navToggle.classList.toggle("naveffect");
+
+
+});
+
+
+
+
+
+
+
+
+
+
+// myfunc = () => {
+//    navToggle.classList.add("naveffect");
+// }
+
+
+
+// about.addEventListener("click", myfunc());
+
+
+
+
+// main.addEventListener("click", () => {
+//     console.log("yusa")
+//     navToggle.classList.add("naveffect");
+// })
+
+
+// turnO = true;
+
+// if(turnO === true){
+
+//     menuIcon.addEventListener('click', () => {
+//         navToggle.classList.toggle("naveffect");
+        
+//     })
+//     turnO =  false;
+// }else if(turnO === false){
+//     body.addEventListener("click", () => {
+//         navToggle.classList.add("naveffect");
+//     })
+// }
+
+
